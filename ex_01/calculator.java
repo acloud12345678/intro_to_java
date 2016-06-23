b189efbea3e8cad0710deb2e13cac3592143c797
Bnobo Bo <acloud1234567@gmail.com>
2016-06-23 20:32:08 +0000

    Finished ex_01

--- ex_01/calculator.java
+++ ex_01/calculator.java
@@ -5,64 +5,95 @@ public class calculator {
         // Math operations: +, -, *, /, %
         // Boolean operations: &&, ||, <, >, <=, >=, ==, !=
         
-        // Passing in an input prompted by console
-        Scanner in = new Scanner(System.in);
-        // Prompt
-        System.out.println("Enter your first value: ");
-        // Variable that stores value
-        int first = in.nextInt();
-        // Prompt
-        System.out.println("Enter your second value: ");
-        // Variable that stores value
-        int second = in.nextInt();
-        // Prompt
-        System.out.println("Enter your operation: ");
-        // Variable that stores value
-        String operation = in.next();
-        
-        double d_result = 0;
-        boolean b_result = false;
-        boolean d_flag = false;
-        boolean b_flag = false;
-        
-        if (operation.equals("+")) {
-            d_result = first + second;
-            d_flag = true;
-        }
-        else if (operation.equals("-")) {
-            d_result = first - second;
-            d_flag = true;
-        }
-        else if (operation.equals("*")) {
-            d_result = first * second;
-            d_flag = true;
-        }
-        else if (operation.equals("/")) {
-            d_result = first / second;
-            d_flag = true;
-        }
-        else if (operation.equals("%")) {
-            d_result = first % second;
-            d_flag = true;
-        }
-        else if (operation.equals("==")) {
-            b_result = first == second;
-            b_flag = true;
-        }
-        
-        
-        System.out.println("\n");
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(operation);
-        if (d_flag == true) {
-            System.out.println(d_result);
-        }
-        else if (b_flag == true) {
-            System.out.println(b_result);
-        }
-        else {
-            System.out.println("No work done, calculation error");
+        // Programs will run indefinitely
+        while (true) {
+            // Passing in an input prompted by console
+            Scanner in = new Scanner(System.in);
+            // Prompt
+            System.out.println("Enter your first value (Enter `Q` to exit): ");
+            // Variable that stores value
+            String input1 = in.next();
+            if (input1.equals("q") || input1.equals("Q")) {
+                return;
+            }
+            // Prompt
+            System.out.println("Enter your second value: ");
+            // Variable that stores value
+            String input2 = in.next();
+            // Prompt
+            System.out.println("Enter your operation: ");
+            // Variable that stores value
+            String operation = in.next();
+            
+            // Gets result and flags for calculations
+            double d_result = 0;
+            boolean b_result = false;
+            boolean d_flag = false;
+            boolean b_flag = false;
+            
+            // Converts a string to an int
+            int first = Integer.parseInt(input1);
+            int second = Integer.parseInt(input2);
+            
+            if (operation.equals("+")) {
+                d_result = first + second;
+                d_flag = true;
+            }
+            else if (operation.equals("-")) {
+                d_result = first - second;
+                d_flag = true;
+            }
+            else if (operation.equals("*")) {
+                d_result = first * second;
+                d_flag = true;
+            }
+            else if (operation.equals("/")) {
+                d_result = first / second;
+                d_flag = true;
+            }
+            else if (operation.equals("%")) {
+                d_result = first % second;
+                d_flag = true;
+            }
+            else if (operation.equals("==")) {
+                b_result = first == second;
+                b_flag = true;
+            }
+            else if (operation.equals("<")) {
+                b_result = first < second;
+                b_flag = true;
+            }
+            else if (operation.equals(">")) {
+                b_result = first > second;
+                b_flag = true;
+            }
+            else if (operation.equals("<=")) {
+                b_result = first <= second;
+                b_flag = true;
+            }
+            else if (operation.equals(">=")) {
+                b_result = first >= second;
+                b_flag = true;
+            }
+            else if (operation.equals("!=")) {
+                b_result = first != second;
+                b_flag = true;
+            }
+    
+            System.out.println("\n");
+            // System.out.println(first);
+            // System.out.println(second);
+            // System.out.println(operation);
+            if (d_flag == true) {
+                System.out.println(d_result);
+            }
+            else if (b_flag == true) {
+                System.out.println(b_result);
+            }
+            else {
+                System.out.println("No work done, calculation error");
+            }
+            System.out.println("\n");
         }
     }
 }
